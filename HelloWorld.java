78607c89db38b0f10c4b7b3f9c346451bd43c169
root <root@ip-172-31-41-174.ap-south-1.compute.internal>
2021-05-18 03:08:46 +0000

    uodated

--- HelloWorld.java
+++ HelloWorld.java
@@ -7,6 +7,5 @@ public class HelloWorld {
     System.out.println("Hello, World");
     System.out.println("Hello, World");
     System.out.println("Hello, World");
-    System.out.println("Hello, World");
   }
 }
